5350351eb2c0b1324d31d904ec20520478607dd9
estebanrfp <estebanrfp@gmail.com>
2023-08-25 23:11:14 +0200

    add main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+import { registerSW } from 'virtual:pwa-register'
+
+import './lib/index'
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh () {
+    // show a prompt to user
+    console.log('New content available, click on reload button to update.')
+  },
+  onOfflineReady () {
+    console.log('show a ready to work offline to user')
+    // show a ready to work offline to user
+  }
+})
+
+updateSW()
